b82817b06fd8f0cd6d071d54e450510a441f5c75
rameela <rameela20@gmail.com>
2017-06-10 14:04:02 +0530

    Finished SignUpController

--- app/src/main/java/com/mit/law/controller/firebase/SignUpController.java
+++ app/src/main/java/com/mit/law/controller/firebase/SignUpController.java
@@ -10,6 +10,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.mit.law.controller.interfaces.OnResponse;
+import com.mit.law.model.ThirdParties;
 import com.mit.law.model.User;
 
 /**
@@ -37,6 +38,37 @@ public class SignUpController {
                     Log.d("SignUp :","task success 1");
                     String userID=mAuth.getCurrentUser().getUid();
                     mDatabase.child(userID).setValue(user);
+                    isSuccessful=true;
+                    responder.responded(isSuccessful);
+
+                }else{
+                    isSuccessful=false;
+                }
+            }
+        });
+
+    }
+
+    public SignUpController(OnResponse onResponse, final User user, final ThirdParties thirdParties) {
+        this.responder=onResponse;
+        mAuth=FirebaseAuth.getInstance();
+        mDatabase= FirebaseDatabase.getInstance().getReference().child("users");
+        mDatabaseThirdParty= FirebaseDatabase.getInstance().getReference().child("thirdParties");
+        String email=user.getEmail();
+        String password=user.getPassword();
+        Log.d("SignUp :","Check 1");
+        mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Log.d("SignUp :","task success 1");
+                    String userID=mAuth.getCurrentUser().getUid();
+                    mDatabase.child(userID).setValue(user);
+                    thirdParties.setTpid(userID);
+                    thirdParties.setRateCount(0);
+                    thirdParties.setReviewAvg(0);
+
+                    mDatabaseThirdParty.child(userID).setValue(thirdParties);
                     isSuccessful=true;
                     responder.responded(isSuccessful);
 
