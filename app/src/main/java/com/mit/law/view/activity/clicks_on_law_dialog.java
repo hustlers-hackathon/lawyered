c1da1e2f0855659a2a2975151a7653cb05815a1a
Shanii\Shani <shanidissanayake11@gmail.com>
2017-06-10 18:47:47 +0530

    Java file for the dialog box of marked as broken laws

---
+++ app/src/main/java/com/mit/law/view/activity/clicks_on_law_dialog.java
@@ -0,0 +1,16 @@
+package com.mit.law.view.activity;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mit.lawyered.R;
+
+public class clicks_on_law_dialog extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.clicks_on_law_dialog);
+    }
+}
