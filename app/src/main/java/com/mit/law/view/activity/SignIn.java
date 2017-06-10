58831921bed8dda8e7e2463d32b6394fe5c3b176
XELVIAS-PC\Ahmed <ahmedifhaam1@gmail.com>
2017-06-10 12:35:41 +0530

    added library

--- app/src/main/java/com/mit/law/view/activity/SignIn.java
+++ app/src/main/java/com/mit/law/view/activity/SignIn.java
@@ -1,12 +1,20 @@
 package com.mit.law.view.activity;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.mit.lawyered.R;
 
 import org.w3c.dom.Text;
@@ -34,9 +42,39 @@ public class SignIn extends AppCompatActivity {
                 checkUser();
             }
         });
+        auth = FirebaseAuth.getInstance();
     }
 
     private void checkUser(){
+        String email = userEmail.getText().toString().trim();//get from field
+        final String pass = passWord.getText().toString();
 
+        //check the whether fields are empty
+        if(TextUtils.isEmpty(email)){
+            Toast.makeText(getApplicationContext(),"Please enter the email address",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(TextUtils.isEmpty(pass)){
+            Toast.makeText(getApplicationContext(),"Enter Password",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        //try signing in
+        auth.signInWithEmailAndPassword(email,pass)
+                .addOnCompleteListener(SignIn.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(!task.isSuccessful()){
+                            //there was an error
+                            Toast.makeText(getApplicationContext(),"Check the inputs ",Toast.LENGTH_SHORT).show();
+
+                        }else{
+                            Intent intent = new Intent(SignIn.this,Home.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                    }
+                })
     }
 }
