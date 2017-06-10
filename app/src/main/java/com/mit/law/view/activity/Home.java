db9d3dac4f06e8d15af2867184b72ab245b0d4f8
XELVIAS-PC\Ahmed <ahmedifhaam1@gmail.com>
2017-06-10 13:47:07 +0530

    small edits

--- app/src/main/java/com/mit/law/view/activity/Home.java
+++ app/src/main/java/com/mit/law/view/activity/Home.java
@@ -1,16 +1,60 @@
 package com.mit.law.view.activity;
 
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.internal.BottomNavigationItemView;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+import com.mit.lawyered.R;
 
 /**
  * Created by Ahmed on 6/10/2017.
  */
 
 public class Home extends AppCompatActivity {
+    BottomNavigationView bttmView;
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+        bttmView = (BottomNavigationView)findViewById(R.id.navView);
+
+
+        //handle user selection on bottom navigation view
+        bttmView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        Fragment selectedFragment = null;
+                        switch (item.getItemId()){
+                            case R.id.menu_home:
+                                selectedFragment=null;
+                                break;
+                            case R.id.menu_notifications:
+                                selectedFragment=null;
+                                break;
+
+                            case R.id.menu_search:
+                                selectedFragment=null;
+                                break;
+                        }
+                        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                        transaction.replace(R.id.frame,selectedFragment);
+                        transaction.commit();
+                        return true;
+                    }
+
+                }
+        );
+
+        //manully display the first fragment when activity loads
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        //transaction.replace(R.id.frame,)//have to select the first fragment
+        transaction.commit();
     }
 
 }
