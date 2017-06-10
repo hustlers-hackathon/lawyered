bf4b308959dd3f6cf9d695bcf4355184979cb890
rameela <rameela20@gmail.com>
2017-06-10 15:04:23 +0530

    Created LawsForTagListController

--- app/src/main/java/com/mit/law/controller/ControlTest.java
+++ app/src/main/java/com/mit/law/controller/ControlTest.java
@@ -2,35 +2,41 @@ package com.mit.law.controller;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
-import com.mit.law.controller.firebase.SignUpController;
+import com.mit.law.controller.firebase.LawsForTagListController;
 import com.mit.law.controller.interfaces.OnResponse;
-import com.mit.law.model.User;
+import com.mit.law.model.Law;
 import com.mit.lawyered.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class ControlTest extends AppCompatActivity implements OnResponse {
 
     boolean signSuccess;
+    List<Law> lawD;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_control_test);
 
-        User user=new User();
-        user.setEmail("nadun@gmail.com");
-        user.setName("Nadun jayakody");
-        user.setPassword("naduns");
-        user.setBalance("Rs.500.00");
-        user.setType("Public");
 
-        SignUpController signUpController=new SignUpController(this,user);
+        List <String>slist=new ArrayList<>();
+        slist.add("Criminal");
+
+        LawsForTagListController lawsForTagListController=new LawsForTagListController(this,slist);
+
+
 
     }
 
+
     @Override
-    public void responded(Object obj) {
-        signSuccess=(boolean)obj;
+    public void responded(Object law) {
+        lawD=(List<Law>)law;
+        Log.d("LAWSIZE2 ",lawD.size()+" ");
     }
 }
