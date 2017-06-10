1861c043218007f142c128c51be1ed870bfdf446
XELVIAS-PC\Ahmed <ahmedifhaam1@gmail.com>
2017-06-10 11:49:58 +0530

    working on signin code

--- app/src/main/java/com/mit/law/view/activity/SignIn.java
+++ app/src/main/java/com/mit/law/view/activity/SignIn.java
@@ -2,14 +2,41 @@ package com.mit.law.view.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.mit.lawyered.R;
 
+import org.w3c.dom.Text;
+
 public class SignIn extends AppCompatActivity {
+
+    TextView userEmail;
+    TextView passWord;
+    Button signIn;
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        userEmail = (EditText) findViewById(R.id.email);
+        passWord = (EditText)findViewById(R.id.password);
+
+        signIn = (Button) findViewById(R.id.btnSignIn);
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkUser();
+            }
+        });
+    }
+
+    private void checkUser(){
+
     }
 }
